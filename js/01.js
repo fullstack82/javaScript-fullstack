9fd27ff615e68a9fd876d2a58a14402e14d660ec
fullstack82 <fullstack8210@gmail.com>
2021-03-08 10:18:31 +0100

    iniciacion a js

---
+++ js/01.js
@@ -0,0 +1,19 @@
+// Variables;
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor;
+
+producto = true; // Reasignando el valor de la variable;
+
+let producto1 = 'Ordenador',
+    disponible1 = true,
+    categoria = 'Informatica';
+
+
+
+// Estilos para las variables;
+let nombre_producto = 'Monitor HD'; // underscore;
+let nombreProducto = 'Monitor HD'; // Camelcase;
+let NombreProducto = 'Monitor HD'; // Pascal case;
+let nombreproducto = 'Monitor HD'; // Lowercase;
+
+console.log(producto)
