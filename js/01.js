9e13a98b27bf81ec00e627c199818ec34666ef4c
fullstack82 <fullstack8210@gmail.com>
2021-03-08 10:24:29 +0100

    creando let y const

--- js/01.js
+++ js/01.js
@@ -1,19 +1,19 @@
-// Variables;
+// Variables (como se solia hacer antes, aun se usa pero menos)
 
-let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor;
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor;
 
 producto = true; // Reasignando el valor de la variable;
 
-let producto1 = 'Ordenador',
+var producto1 = 'Ordenador',
     disponible1 = true,
     categoria = 'Informatica';
 
 
 
 // Estilos para las variables;
-let nombre_producto = 'Monitor HD'; // underscore;
-let nombreProducto = 'Monitor HD'; // Camelcase;
-let NombreProducto = 'Monitor HD'; // Pascal case;
-let nombreproducto = 'Monitor HD'; // Lowercase;
+var nombre_producto = 'Monitor HD'; // underscore;
+var nombreProducto = 'Monitor HD'; // Camelcase;
+var NombreProducto = 'Monitor HD'; // Pascal case;
+var nombreproducto = 'Monitor HD'; // Lowercase;
 
 console.log(producto)
---
+++ js/02.js
@@ -0,0 +1,11 @@
+// Variables con Let; (nueva manera de declarar variables correctamente);
+
+let producto = 'Portatil Lennovo'; // Iniciar variable y asignarle un valor;
+
+producto = true; // Reasignando el valor de la variable;
+
+let producto1 = 'Ordenador de ultima generación',
+    disponible1 = true, 
+    categoria = 'Informatica';
+
+console.log(producto1, disponible1, categoria) // Nos muestra los datos por consola
